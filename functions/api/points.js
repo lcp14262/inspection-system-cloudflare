7035ea7aea0511e0b1b5c8c9a56bfa1e9136db09
lcp14262 <108786008+lcp14262@users.noreply.github.com>
2026-05-09 21:24:23 +0800

    Update points.js

--- functions/api/points.js
+++ functions/api/points.js
@@ -1,7 +1,7 @@
 // functions/api/points.js
 export async function onRequest(context) {
     const CHECKIN_POINTS = {
-       'A001': { name: '1号厂房东侧', area: '芜湖工厂', lat: 31.230834, lng: 118.173690, radius: 100 },
+       'A001': { name: '1号大门', area: '芜湖工厂', lat: 31.230834, lng: 118.173690, radius: 100 },
          'A002': { name: '1号大门', area: '合肥工厂', lat: 30.5215, lng: 117.0478, radius: 200 },
          'B001': { name: '1号大门', area: '安庆工厂', lat: 31.7608, lng: 117.2027, radius: 200 },
     };
